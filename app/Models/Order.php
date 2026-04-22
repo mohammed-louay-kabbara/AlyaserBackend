b5952f4e86703a814817863c5af8d436d0282879
Doha05a <mohamedlouayk@gmail.com>
2026-04-22 22:22:50 +0300

    deliver

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
     {
         protected $fillable = [
             'user_id', 'total_amount', 
-            'status', 'notes', 'ameen_guid', 'is_synced'
+            'status', 'notes', 'ameen_guid', 'is_synced','problem'
         ];
 
     public function items()
