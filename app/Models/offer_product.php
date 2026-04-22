3ffdcae27b12b3461a8ed5b427c6eaa126ec57ba
Doha05a <mohamedlouayk@gmail.com>
2026-04-22 20:38:37 +0300

    offer

--- app/Models/offer_product.php
+++ app/Models/offer_product.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class offer_product extends Model
 {
-    //
+    protected $fillable = ['offer_id', 'product_id', 'quantity'];
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
